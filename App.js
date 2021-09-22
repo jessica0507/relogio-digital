7674d33b4b54d0071e0d2ff8b09a81319e1aa14b
Jessica Silva <jessicasilva552santos@gmail.com>
2021-09-22 10:43:29 -0300

    Primeiro Commit
    Primeiro Commit

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import Moment from 'react-moment';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+          <View style={styles.time}>
+            <Moment format="hh:mm:ss" interval={1000} />
+          </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+      container: {
+        flex: 1,
+        backgroundColor: '#000',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+      time: {
+        color: '83d38d',
+        textShadowColor: '#00ff00',
+        fontSize: 70,
+        textShadowOffset: {
+          width: 0,
+          height: 0
+        },
+        textShadowRadius: 5
+      }
 });
